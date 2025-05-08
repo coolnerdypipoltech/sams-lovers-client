5e455f9418459ae95d35c4a98d1fbd28dcd50a2f
Dylan Ocampo <dylanomar@live.com.mx>
2025-05-08 17:15:08 -0600

    Login System

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,74 @@
 import "../styles/Login.css";
 import logo from "../assets/samsLogo.webp";
-import { useState } from "react";
+import { useState, useContext, useRef } from "react";
 import  CreatePage  from "../subPages/CreatePage"
 import  PasswordPage  from "../subPages/PasswordPage";
+import { ElementContextRoute } from "../context/RouteContext";
+import { LogIn } from "../hooks/apicalls";
 function Login() {
-
+  const { setLoginToken } = useContext(ElementContextRoute);
   const [subPage, setSubPage] = useState("");
-
+  const [errorPassword, setErrorPassword] = useState(false)
+  const [errorEmail, setErrorEmail] = useState(false)
+  const LoginText = useRef("")
+  const LoginPassword = useRef("")
   let subPageContent = null
 
   if(subPage === "Password"){
     subPageContent = <PasswordPage></PasswordPage>
   }
-  
   if(subPage === "Create"){
     subPageContent = <CreatePage></CreatePage>
   } 
+
+  const onClickLogin = async () =>  {
+    if(inputValidation()){
+      const response = await LogIn(LoginText.current.value, LoginPassword.current.value);
+      if(response){
+        await setLoginToken(response.token);
+      }
+    }
+
+  }
+
+  const inputValidation = () =>{
+    const response1 = ValidateEmail()
+    const response2 = ValidatePassword()
+
+    if(response1 === true && response2 === true){
+      return true
+    }else{
+      return false
+    }
+
+  }
+
+
+  const ValidatePassword = async () => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    if(passwordRegex.test(LoginText.current.value)){
+      if(errorPassword === true){
+        setErrorPassword(false)
+      }
+      return true
+    }else{
+      setErrorPassword(true)
+      return false
+    }
+  }
+
+  const ValidateEmail = async () => {
+    const strictEmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    if(strictEmailRegex.test(LoginText.current.value)){
+      if(errorEmail === true){
+        setErrorEmail(false)
+      }
+      return true
+    }else{
+      setErrorEmail(true)
+      return false
+    }
+  }
 
   return (
     <>
@@ -27,14 +80,23 @@ function Login() {
         </div>
 
         <div className="LoginMenuContainer">
-        <input placeholder="Nombre" className="inputLoginMenu"></input>
+        <input placeholder="Correo" ref={LoginText} className="inputLoginMenu"></input>
+        {errorEmail === true ? (
+        <span>Porfavor verique su correo electronico</span>
+        ) : (
+          <></>
+        )}
         <div className="passwordContainer">
-        <input placeholder="Correo" className="inputLoginMenu"></input>
+        <input placeholder="Contraseña" ref={LoginPassword} className="inputLoginMenu"></input>
+        {errorPassword=== true ? (
+        <span>Porfavor verique su contraseña</span>
+        ) : (
+          <></>
+        )}
         <p className="PasswordTextLogin">Olvidé mi contraseña</p></div>
-        
         <div className="LoginMenuBottomContainer">
-          
-          <p>Acceder</p>
+
+          <p onClick={onClickLogin}>Acceder</p>
           <p>¿NO TIENES CUENTA? CREA UNA</p>
         </div>
       </div>
