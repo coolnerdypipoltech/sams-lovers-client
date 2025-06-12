b670a6a7c2fb1b439ca32453a810d2f8585b5798
Javier Serrano <javierserranosolis@rckgames.com>
2025-06-12 17:02:33 -0600

    Start codes

--- src/pages/Codes.js
+++ src/pages/Codes.js
@@ -1,9 +1,55 @@
-import { useState } from "react";
-
+import { ExchangeCode } from "../hooks/apicalls";
+import { ElementContextData } from "../context/DataContext";
+import { ElementContextRoute } from "../context/RouteContext";
+import { useContext, useRef } from "react";
 import "../styles/Codes.css";
 
 function Codes() {
-  const [openArticlePage, setOpenArticlePage] = useState(true);
+  const { changeRoute } = useContext(ElementContextRoute);
+  const { UserData, setNewUserDiamonds } = useContext(ElementContextData);
+
+  let code = useRef("");
+
+  const handleOnChangeInput = (input) => {
+    code.current = input;
+  }
+
+  const handleExchangeCode = async () => {
+
+    if (code.current === "") return;
+
+    const response = await ExchangeCode(`${UserData.current.token_type} ${UserData.current.access_token}`,code.current);
+    const data = await response.json();
+    console.log(data);
+    if (response.ok) {
+        setNewUserDiamonds(data.user.related.diamonds);
+      } else {
+              if (data.message) {
+        switch(data.message) {
+          case "api.error.unauthorized":
+            changeRoute("Login");
+            break;
+          case " api.error.code_not_found":
+            //changeRoute("Login");
+            break;
+          case "api.error.code_expired":
+            //openMaxPurchasesReachedPopUp();
+            break;
+          case " api.error.max_redemptions_reached":
+            //openNoDiamondsPopUp();
+            break;
+          case " api.error.no_stock":
+            break;
+          default:
+            //openGeneralErrorPopUp();
+            break;
+        }
+      }else{
+        //openGeneralErrorPopUp();
+      }
+      return;
+      }
+  }
 
   return (
     <>
@@ -17,15 +63,12 @@ function Codes() {
         <div className="Divider"></div>
         <div className="CodeInputContainer">
 
-            <p className="CodeInputText"> Introduce tu código de 16 digitos</p>
-          <input placeholder="Código" className="GeneralInput"></input>
-
-          
+            <p className="CodeInputText"> Introduce tu código</p>
+          <input placeholder="Código" className="GeneralInput" onChange={e => handleOnChangeInput(e.target.value)}></input>
         </div>
         <div className="CodeButtonContainer">
-          <button className="GeneralButton4">Canjear código</button>
+          <button className="GeneralButton4" onClick={handleExchangeCode}>Canjear código</button>
         </div>
-        
       </div>
     </>
   );
